b90420986294c71aa4d8abafb75c308fe978a391
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-01-24 19:00:36 -0700

    completed the background color and cheat mode extensions FTW

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,14 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"].to_i
+  cheat = params["cheat"]
   message = check_guess(guess)
-  erb :index, :locals => {:message => message}
+  cheat_message = check_for_cheat(cheat)
+  color = check_guess_for_color(guess)
+  erb :index, :locals => {:message => message,
+                          :color => color,
+                          :cheat_message => cheat_message
+                         }
 end
 
 def check_guess(guess)
@@ -20,5 +26,25 @@ def check_guess(guess)
     "Way too low!"
   elsif guess < SECRET_NUMBER
     "Too low!"
+  end
+end
+
+def check_guess_for_color(guess)
+  if guess >= SECRET_NUMBER + 5
+    "red"
+  elsif guess > SECRET_NUMBER
+    "pink"
+  elsif guess == SECRET_NUMBER
+    "white"
+  elsif guess <= SECRET_NUMBER - 5
+    "red"
+  elsif guess < SECRET_NUMBER
+    "pink"
+  end
+end
+
+def check_for_cheat(cheat)
+  if cheat
+    "The SECRET NUMBER is #{SECRET_NUMBER}"
   end
 end
